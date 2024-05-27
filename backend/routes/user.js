659fe7405d67d390c0a1e49caa584239e05f8b8f
Piyushagrawal1 <agarwalpiyush166@gmail.com>
2024-05-27 21:01:54 +0530

    chore: Add signup route for user registration

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,56 @@
 // backend/routes/user.js
 const express = require('express');
+const zod = require('zod')
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require("../config");
 
 const router = express.Router();
+
+//signup and signin routes
+
+const signupBody = zod.object({
+    username: zod.string().email(),
+    firstName: zod.string(),
+    lastName: zod.string(),
+    password: zod.string(),
+})
+
+router.post('/signup', async (req, res) => {
+    const { success } = signupBody.safeParse(req.body)
+
+    if (!success) {
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const existingUser = await User.findOne({
+        username: req.body.username
+    })
+
+    if (existingUser) {
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const user = await User.create({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password
+    })  
+
+    const userId = user._id;
+
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET)
+
+    res.json({
+        message: "User created successfully",
+        token: "jwt"    
+    })
+})
 
 module.exports = router;
