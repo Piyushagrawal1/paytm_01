af579caf76b96ed57c57badaf8d6e8b9c8f24a7f
Piyushagrawal1 <agarwalpiyush166@gmail.com>
2024-05-27 20:44:09 +0530

    chore: Add configuration file for backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const rootRouter = require('./routes/index')
+const cors = require('cors')
+
+app.use(cors())
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1', rootRouter) 
+
+
+app.listen(3000);
